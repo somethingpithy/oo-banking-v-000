a8103fafd2619eed8997ca404a10ab05f4e3a526
Carlota Bindner <cev.bindner@gmail.com>
2017-09-21 17:50:55 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,10 @@ class Transfer
   end
 
   def reverse_transfer
-
+    if self.status = "complete"
+      sender.balance += amount
+      receiver.balance -= amount
+      self.status = "reversed"
   end
 
 end
