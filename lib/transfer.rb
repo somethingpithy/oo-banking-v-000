7a12502cffd5f60601d5d714c2d4cb3940f6235c
Carlota Bindner <cev.bindner@gmail.com>
2017-09-21 17:52:29 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,8 +19,7 @@ class Transfer
       receiver.balance += amount
       self.status = "complete"
     else
-      self.status = "rejected"
-      "Transaction rejected. Please check your account balance."
+      reject_transfer
     end
   end
 
@@ -29,7 +28,13 @@ class Transfer
       receiver.balance -= amount
       sender.balance += amount
       self.status = "reversed"
+    else
+      reject_transfer
     end
   end
 
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
 end
