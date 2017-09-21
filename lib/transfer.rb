d5733344c296dfa8e55d4e23cd481dee54de2759
Carlota Bindner <cev.bindner@gmail.com>
2017-09-21 17:45:28 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,7 @@
 class Transfer
-  attr_accessor :sender, :receiver, :amount, :status
-
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount
+  
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
@@ -9,7 +10,7 @@ class Transfer
   end
 
   def valid?
-
+    
   end
 
   def execute_transaction
