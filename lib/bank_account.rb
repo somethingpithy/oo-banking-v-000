de35e530e4d3a2955a42bce682e8244076e51a0a
Carlota Bindner <cev.bindner@gmail.com>
2017-09-21 17:31:22 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -8,4 +8,14 @@ class BankAccount
     @status = open
   end
 
+  def deposit(amount)
+    self.balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  
+  
 end
